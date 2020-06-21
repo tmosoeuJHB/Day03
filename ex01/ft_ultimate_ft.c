35687f0700010432753238491c1c3f92b11c2649
Tiro Mosoeu <tmosoeu@c4r7s10.wethinkcode.co.za>
2020-06-21 13:52:59 +0200

    ex01 is done

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmosoeu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 13:41:43 by tmosoeu           #+#    #+#             */
+/*   Updated: 2020/06/21 13:51:39 by tmosoeu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void ft_ultimate_ft(int *********nbr);
+
+void ft_ultimate_ft(int *********nbr) {
+	int x;
+	int * nbr1;
+	int **nbr2;
+	int ***nbr3;
+	int ****nbr4;
+	int *****nbr5;
+	int ******nbr6;
+	int *******nbr7;
+	int ********nbr8;
+
+	x = 42;
+	nbr1 = &x;
+	nbr2 = &nbr1;
+	nbr3 = &nbr2;
+	nbr4 = &nbr3;
+	nbr5 = &nbr4;
+	nbr6 = &nbr5;
+	nbr7 = &nbr6;
+	nbr8 = &nbr7;
+	nbr = &nbr8;
+}
